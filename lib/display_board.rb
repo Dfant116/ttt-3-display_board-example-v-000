84393c7c449e829331f169c131f6b7b0517fbc9f
Dalton Fant <daltonfant16@gmail.com>
2017-12-28 19:51:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board = " "|" "|" "-----------" "|" "|" "-----------" "|" "|" "
+display_board = "   |   |   "br"-----------"br"   |   |   "br"-----------"br"   |   |   "
 
 puts display_board
